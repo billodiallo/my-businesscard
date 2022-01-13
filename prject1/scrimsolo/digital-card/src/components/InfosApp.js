7c73c9813cdc2c0c8c7be1df5984540a470a7d93
Billo Diallo <billodiallonet@gmail.com>
2022-01-13 15:50:18 +0300

    Add image css properties

--- prject1/scrimsolo/digital-card/src/components/InfosApp.js
+++ prject1/scrimsolo/digital-card/src/components/InfosApp.js
@@ -1,8 +1,9 @@
 import macky from "../images/macky.jpg"
+import '../styles/styles.css'
 const Info = () => {
 return(
   <div>
-  <img src = {macky} alt= "m"/>
+  <img src = {macky} alt= "m" className="photo"/>
 
 </div>
 
