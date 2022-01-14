19931364ca1ec4188fa71c646985f2b93132dd91
Billo Diallo <billodiallonet@gmail.com>
2022-01-14 11:24:31 +0300

    Add social media icon,create InfosApp css module,changing styling

--- prject1/scrimsolo/digital-card/src/components/InfosApp.js
+++ prject1/scrimsolo/digital-card/src/components/InfosApp.js
@@ -1,9 +1,18 @@
-import macky from "../images/macky.jpg"
+import macky from "../assets/profile.jpg"
 import '../styles/styles.css'
+import  styleinfo from '../styles/InfosApp.module.css'
 const Info = () => {
 return(
-  <div>
-  <img src = {macky} alt= "m" className="photo"/>
+  <div className={styleinfo.info1}>
+
+    <header>
+    <img src = {macky} alt= "m" className={styleinfo.photo}/>
+    </header>
+  
+  <h1 className={styleinfo.head1}>Macky Sall</h1>
+  <p className={styleinfo.frontend}>Front end Developer</p>
+  <small>text not visible now</small>
+
 
 </div>
 
