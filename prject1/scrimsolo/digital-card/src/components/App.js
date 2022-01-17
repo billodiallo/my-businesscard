f67479c2154551d13403fa7f3982feb6748221be
Billo Diallo <billodiallonet@gmail.com>
2022-01-17 16:20:44 +0300

    Update Interest component, add Interest css module

--- prject1/scrimsolo/digital-card/src/components/App.js
+++ prject1/scrimsolo/digital-card/src/components/App.js
@@ -1,5 +1,6 @@
 import Info from "./InfosApp";
 import About from '../components/About'
+import Interest from "./Interest";
 import '../components/App.css'
 
 const  App = () => {
@@ -7,6 +8,7 @@ const  App = () => {
     <div className="App">
     <Info />
     < About/>
+    <Interest />
     </div>
   );
 }
---
+++ prject1/scrimsolo/digital-card/src/components/Interest.js
@@ -0,0 +1,16 @@
+import styleinterest from '../styles/Interest.module.css'
+
+const Interest = () => {
+
+    return (
+        <main className={styleinterest.interest}>
+       <h1 className={styleinterest.header1}>Interest</h1>
+       <p className={styleinterest.text1}>Web accessibility, Cloud environment, Badminton, Animation and anime, Tea lover</p>
+        </main>
+
+
+    );
+
+}
+
+export default Interest;
