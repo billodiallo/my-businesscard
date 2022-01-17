b35bda8bb7f989a6b4b8ad7d4a29432dc21099d3
Billo Diallo <billodiallonet@gmail.com>
2022-01-17 12:50:28 +0300

    update info, style social icons

--- prject1/scrimsolo/digital-card/src/components/InfosApp.js
+++ prject1/scrimsolo/digital-card/src/components/InfosApp.js
@@ -1,23 +1,43 @@
 import macky from "../assets/profile.jpg"
+import emailLogo from "../assets/Email-logo.png"
+import linkedln from '../assets/LinkedIn-logo.png'
 import '../styles/styles.css'
 import  styleinfo from '../styles/InfosApp.module.css'
 const Info = () => {
-return(
-  <div className={styleinfo.info1}>
-
-    <header>
-    <img src = {macky} alt= "m" className={styleinfo.photo}/>
-    </header>
-  
-  <h1 className={styleinfo.head1}>Macky Sall</h1>
-  <p className={styleinfo.frontend}>Front end Developer</p>
-  <small>text not visible now</small>
-
-
-</div>
-
-);
- 
+  return (
+    <div className={styleinfo.container}>
+      <header className={styleinfo.header}>
+        <img
+          className={styleinfo.avatar}
+          src={macky}
+          alt="macky"
+        />
+      </header>
+      <div className={styleinfo.bio}>
+        <h1 className={styleinfo.name}>Macky</h1>
+        <p className={styleinfo.title}>Frontend Developer</p>
+        <p className={styleinfo.small}>(www.info)</p>
+        <div className={styleinfo.contacts}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styleinfo.links} ${styleinfo.email}`}
+            href="mailto:billoaq@sss.com"
+          >
+            <img className={styleinfo.icons} src={emailLogo} alt="" /> Email
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styleinfo.links} ${styleinfo.linkedin}`}
+            href="https://www.linkedin.com/in/macky"
+          >
+            <img className={styleinfo.icons} src={linkedln} alt="" />
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default Info;
